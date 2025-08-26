389c80aae869d90492fc04474a7d6aaa7b2486f9
Zakaria MAIT <95222260+MTZACK9@users.noreply.github.com>
2025-08-26 11:52:10 +0100

    Add errors to parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,25 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/mtzack9/the-interpreter/ast"
 	"github.com/mtzack9/the-interpreter/lexer"
 	"github.com/mtzack9/the-interpreter/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
-
+	l         *lexer.Lexer
+	errors    []string
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -82,10 +87,20 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 }
 
 func (p *Parser) expectPeek(t token.TokenType) bool {
-	if (p.peekTokenIs(t)) {
+	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
